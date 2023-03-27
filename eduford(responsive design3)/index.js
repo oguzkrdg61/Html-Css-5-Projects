3dd2bd9098592615ea6ef929510b9b3c6610fced
Oğuzhan Karadağ <66627470+oguzkrdg61@users.noreply.github.com>
2023-03-27 20:23:43 +0300

    Add files via upload

---
+++ eduford(responsive design3)/index.js
@@ -0,0 +1,9 @@
+const navLinks = document.getElementById("navLinks");
+
+// Toggle Menu
+function showMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
